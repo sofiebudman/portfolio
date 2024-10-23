fa21f608ff53142902e960de0b2d3306a2d1cc67
Sofie Budman <changbudmanfamily@gmail.com>
2024-10-23 11:59:19 -0700

    ml readme

---
+++ Machine_Learning_Algorithms/Machine_Learning_Algorithms.md
@@ -0,0 +1,37 @@
+# **Machine Learning Algorithms**
+
+### Overview
+
+-   Created code for multiple machine learning algorithms
+
+-   Data from the R data package
+
+### Packages/Requirements
+
+Install Required Packages
+
+``` R
+install.packages("caret")
+install.packages("class")
+install.packages("tidyverse")
+install.packages('neuralnet')
+install.packages('keras')
+install.packages('tensorflow')
+install.packages('ggplot2')
+install.packages('cowplot')
+install.packages('randomForest')
+```
+
+Load Required Packages
+
+``` R
+library(caret)
+library(class) 
+library(tidyverse)
+library(neuralnet)
+library(keras)
+library(tensorflow)
+library(ggplot2)
+library(cowplot)
+library(randomForest)
+```
--- Machine_Learning_Algorithms/k_nearest_neighbors.R
+++ Machine_Learning_Algorithms/k_nearest_neighbors.R
@@ -2,8 +2,21 @@ library(caret)
 library(class) 
 library(tidyverse)
 
+data <- iris
+iris$Species <- as.numeric(factor(iris$Species)) - 1
 set.seed(123)
-model_knn <- knn(knnTrain, knnTest,cl = knnTrain$SMAR, k = KNEIGHBORS)
+hKNEIGHBORS <- 5
+
+# Shuffle the dataset and create an index for splitting
+shuffled_indices <- sample(1:nrow(data), nrow(data))
+train_indices <- shuffled_indices[1:round(train_ratio * nrow(data))]
+test_indices <- shuffled_indices[(round(train_ratio * nrow(data)) + 1):nrow(data)]
+
+# Split the data
+train <- data[train_indices, ]
+test <- data[test_indices, ]
+
+model_knn <- knn(train, test,cl = train$Species, k = KNEIGHBORS)
 cf<- table(knnTest$SMAR, model_knn)
 accuracy_knn <- sum(diag(cf))/nrow(knnTest)
 print(accuracy_knn)
